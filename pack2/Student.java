10e11015d407bd465da4ab018d4027681619aff6
Shreya4net <pujakumari.civil@gmail.com>
2016-12-28 00:11:11 +0530

    java session 4 assignment1
    java session4 assignment

---
+++ pack2/Student.java
@@ -0,0 +1,14 @@
+package pack2;
+import pack3.C;
+
+public class Student {
+	public static void main(String args[])
+	{
+		C c1=new C();
+		System.out.println(c1.multiply(6,5));
+	
+	}
+	
+}
+
+
---
+++ pack3/C.java
@@ -0,0 +1,13 @@
+package pack3;
+
+public class C {
+
+protected int sum(int x,int y)
+{
+	return(x+y);
+}
+public int multiply(int x,int y)
+{
+	return(x*y);
+}
+}
